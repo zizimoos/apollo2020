dbb07690cfb4f9ea6406d56b526cfe946e9810c4
henry <zizimoos@gmail.com>
2020-09-23 03:46:36 +0900

    ....

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,27 @@
-import React from "react";
-export default () => "Detail";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  console.log(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
